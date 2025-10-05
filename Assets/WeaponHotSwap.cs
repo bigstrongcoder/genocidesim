cf02a59bf4ca0d304c99e3f64e8bf7c994f316bc
shreyasaga2011-lang <shreyasaga2011@gmail.com>
2025-10-05 17:19:31 -0500

    Revert "Merge branch 'main' of https://github.com/bigstrongcoder/genocidesim"
    This reverts commit 308f07335a5ae2ad7eccf0680d63fa896ed02f09, reversing
    changes made to 17eac807aef3b2a680d5fcc93b9bf7e2637ea5bb.

--- Assets/WeaponHotSwap.cs
+++ Assets/WeaponHotSwap.cs
@@ -6,8 +6,7 @@ public class WeaponHotSwap : MonoBehaviour
     private PlayerShooting playerShooting;
     private DoublePumpShotgun doublePumpShotgun;
 
-    [Header("Weapon Objects")]
-    public Transform weaponPivot;                // Assign this in the Inspector
+    [Header("Weapon Sprites")]
     public GameObject playerShootingGunSprite;
     public GameObject doublePumpGunSprite;
 
@@ -18,18 +17,21 @@ public class WeaponHotSwap : MonoBehaviour
         playerShooting = GetComponent<PlayerShooting>();
         doublePumpShotgun = GetComponent<DoublePumpShotgun>();
 
+        // Start with PlayerShooting
         SetActiveWeapon(1);
     }
 
     void Update()
     {
+        // --- Handle Hot-Swap ---
         if (Input.GetKeyDown(KeyCode.Alpha1))
             SetActiveWeapon(1);
         else if (Input.GetKeyDown(KeyCode.Alpha2))
             SetActiveWeapon(2);
 
-        if (weaponPivot != null)
-            AimGunAtMouse(weaponPivot);
+        // --- Make active gun follow mouse ---
+        if (activeGun != null)
+            AimGunAtMouse(activeGun);
     }
 
     void SetActiveWeapon(int weaponNumber)
@@ -39,28 +41,29 @@ public class WeaponHotSwap : MonoBehaviour
         playerShooting.enabled = usingPlayerShooting;
         doublePumpShotgun.enabled = !usingPlayerShooting;
 
+        // Toggle sprites
         if (playerShootingGunSprite) playerShootingGunSprite.SetActive(usingPlayerShooting);
         if (doublePumpGunSprite) doublePumpGunSprite.SetActive(!usingPlayerShooting);
 
+        // Set active gun transform
         activeGun = usingPlayerShooting
             ? playerShootingGunSprite?.transform
             : doublePumpGunSprite?.transform;
+
+        Debug.Log(usingPlayerShooting ? "Switched to PlayerShooting" : "Switched to DoublePumpShotgun");
     }
 
-    void AimGunAtMouse(Transform pivot)
+    void AimGunAtMouse(Transform gun)
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePos - pivot.position;
+        Vector2 direction = mousePos - gun.position;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        pivot.rotation = Quaternion.Euler(0f, 0f, angle);
+        gun.rotation = Quaternion.Euler(0f, 0f, angle);
 
-        // Flip gun sprite without altering scale
-        if (activeGun != null)
-        {
-            SpriteRenderer sr = activeGun.GetComponent<SpriteRenderer>();
-            if (sr != null)
-                sr.flipY = direction.x < 0;
-        }
+        // Flip sprite vertically if aiming left
+        Vector3 localScale = gun.localScale;
+        localScale.y = direction.x < 0 ? -1 : 1;
+        gun.localScale = localScale;
     }
 }
